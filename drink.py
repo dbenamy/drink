c4c143d5ecbbb7ac8d654cbc83f5f0e402874bef
Daniel Benamy <daniel@benamy.info>
2012-09-07 11:15:25 -0400

    Replaced playlists with library shuffle. Moved top level code into class.

--- drink.py
+++ drink.py
@@ -10,36 +10,35 @@ from dirwatcher import DirWatcher
 from mainwindow import MainWindow
 from playlist import Playlist
 
+class DrinkApp(QtGui.QApplication):
+    def __init__(self, argv):
+        super(DrinkApp, self).__init__(argv)
+        
+        self.__window = MainWindow()
+        self.__player = audio.Player()
+        self.__dirWatcher = DirWatcher('/Users/dbenamy/Music')
+        self.__db = DB()
+        # self.__playlist = None
+        # playlist = Playlist()
+
+        # playlist.newSong.connect(lambda song: logging.debug('playlist new song %s' % song))
+        # playlist.done.connect(lambda: logging.debug('playlist done'))
+        self.__player.songDone.connect(lambda: logging.debug('player song done'))
+        self.__window.newPlaylist.connect(lambda songs: logging.debug('window new playlist %s' % songs))
+        self.__window.controls.playButton.clicked.connect(lambda: logging.debug('window play'))
+        self.__window.controls.nextButton.clicked.connect(lambda: logging.debug('window next'))
+
+        # playlist.newSong.connect(self.__player.play_file)
+        # playlist.done.connect(self.__player.stop)
+        self.__player.songDone.connect(self.playNextRandSong)
+        # self.__window.newPlaylist.connect(playlist.setSongs)
+        self.__window.controls.playButton.clicked.connect(self.playNextRandSong)
+        self.__window.controls.nextButton.clicked.connect(self.playNextRandSong)
+
+    def playNextRandSong(self):
+        self.__player.play_file(self.__db.randSong())
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-
-    app = QtGui.QApplication(sys.argv)
-    
-    window = MainWindow()
-    playlist = Playlist()
-    player = audio.Player()
-    dirWatcher = DirWatcher('/Users/dbenamy/Music')
-    db = DB()
-
-    if db.numSongs() == 0:
-        print("There are no songs in the database. We'll wait 1/2 a sec to see "
-              "if some show up so we can play them.")
-        time.sleep(0.5)
-
-    playlist.newSong.connect(lambda song: logging.debug('playlist new song %s' % song))
-    playlist.done.connect(lambda: logging.debug('playlist done'))
-    player.songDone.connect(lambda: logging.debug('player song done'))
-    window.newPlaylist.connect(lambda songs: logging.debug('window new playlist %s' % songs))
-    window.controls.nextButton.clicked.connect(lambda: logging.debug('window next'))
-    window.controls.playButton.clicked.connect(lambda: logging.debug('window play'))
-
-    playlist.newSong.connect(player.play_file)
-    playlist.done.connect(player.stop)
-    player.songDone.connect(playlist.next)
-    window.newPlaylist.connect(playlist.setSongs)
-    window.controls.nextButton.clicked.connect(playlist.next)
-    # TODO rand "playlist"
-    window.controls.playButton.clicked.connect(lambda: player.play_file(db.randSong()))
-
-    ret = app.exec_()
-    sys.exit(ret)
+    app = DrinkApp(sys.argv)
+    sys.exit(app.exec_())
